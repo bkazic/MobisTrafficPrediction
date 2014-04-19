ddeb7dd01848c5f1d46dda7bd8cfc14f46602bc0
bkazic <blaz.kazic@ijs.si>
2014-04-19 18:35:55 +0200

    First commit. Load two stores and load from sandbox. countersNodes.txt and measurements_0016_21.txt Print first 10 records from created store

---
+++ src/sensors.js
@@ -0,0 +1,56 @@
+var analytics = require('analytics');
+
+
+// Loading store for counter Nodes
+var filename_counters_n = "./sandbox/sensors/countersNodes.txt";
+var CounterNode = qm.store("CounterNode");
+qm.load.jsonFile(CounterNode, filename_counters_n);
+
+// Define measurement store definition as a function so that it can be used several times 
+var measurementStoresDef = function (storeName, extraFields) {
+   storeDef = [{
+       "name" : storeName,
+       "fields" : [
+            { "name" : "DateTime", "type" : "datetime" },
+            { "name" : "NumOfCars", "type" : "float", "null" : true },
+            { "name" : "Gap", "type" : "float", "null" : true },
+            { "name" : "Occupancy", "type" : "float", "null" : true },
+            { "name" : "Speed", "type" : "float", "null" : true },
+            { "name" : "TrafficStatus", "type" : "float", "null" : true }
+        ],
+        "joins" : [
+            { "name" : "measuredBy", "type" : "field", "store" : "CounterNode" }
+        ]
+  }];
+  if(extraFields) {
+    storeDef[0].fields = storeDef[0].fields.concat(extraFields);
+  }
+  qm.createStore(storeDef);
+};
+
+// Find measurement files
+var fileList = fs.listFile("./sandbox/sensors/","txt");
+var keyWord = "measurements_"; // Keyword, by which to find files
+var fileListMeasures = fileList.filter( function(element) {return element.indexOf(keyWord) >= 0;}); // Find files with keyword in name
+var measurementIds = fileListMeasures.map( function(element) {return element.substring(element.length-12,element.length-4);}); // Extract IDs from file names
+
+// Load measurement files to stores
+for (var i=0; i<fileListMeasures.length; i++) {
+  // Creating Store for measurements
+  var storeName = "CounterMeasurement"+measurementIds[i];
+  measurementStoresDef(storeName);
+
+  // Load measurement files to created store
+  var store = qm.store(storeName);
+  qm.load.jsonFile(store, fileListMeasures[i]);
+}
+
+// Open the first two stores
+var testStore = qm.store(qm.getStoreList()[1].storeName);
+var records = testStore.recs;
+console.say("Print few record from the loaded store: " + testStore.name);
+for (var ii=0; ii<10; ii++) {
+  console.say("Store List: " + JSON.stringify(records[ii]));
+}
+
+
